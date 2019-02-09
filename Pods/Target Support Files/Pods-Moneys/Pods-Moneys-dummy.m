937b604442a56b7ad5d91ac1dc0577160c1431a5
Arthur Papailhau <papailhau@uber.com>
2019-02-09 20:30:59 +0100

    Adds .gitignore

--- Pods/Target Support Files/Pods-Moneys/Pods-Moneys-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Moneys : NSObject
-@end
-@implementation PodsDummy_Pods_Moneys
-@end
--- Pods/Target Support Files/Pods-MoneysTests/Pods-MoneysTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoneysTests : NSObject
-@end
-@implementation PodsDummy_Pods_MoneysTests
-@end
